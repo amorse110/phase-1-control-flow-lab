c5e35ff1fa00379cf79061815b1029ba5c46bc82
Austin Morse <amorse110@gmail.com>
2023-05-22 12:03:21 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!'
+  }
+  else if (ride >= 2500) {
+    return 'No can do.'
+  }
+  else if (ride >= 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (ride >= 400) {
+    return 'That will be twenty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let cityMessage
+cityMessage = city === "NYC" ? "Ok, sounds good." : "No go.";
+return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye."
+}
 }
